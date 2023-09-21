836cc6f8ab6fb0cb067b0f6c982c240e16c1b30b
FabsPanda <75232387+FabsPanda@users.noreply.github.com>
2023-09-21 21:35:01 +0700

    first commit

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import Hubert from "../public/hubert.jpg";
+import Hubert from "../public/hubert2.png";
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
